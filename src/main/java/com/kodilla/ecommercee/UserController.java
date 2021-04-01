b4cc829ebd2a8637f85372692c87729be270b4f5
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-04-01 23:06:34 +0200

    UserController

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,25 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    public void createUser(UserDto userDto) {
+        //do nothing
+    }
+
+    public void deleteUser(UserDto userDto) {
+        //do nothing
+    }
+
+    public void editUser(UserDto userDto) {
+        //do nothing
+    }
+
+    public void getUser(Long userId) {
+        //do nothing
+    }
+
 }
