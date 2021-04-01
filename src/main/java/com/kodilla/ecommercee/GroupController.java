6efca5def42c461097741964d7b464dfd675d217
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-01 16:40:09 +0200

    GroupController
    - wstępna implementacja GroupController wykonana,
    - build.gradle -> lombok, procesor  adnotacji
    - domain.GroupDto -> dodatno Dto grup

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.GroupDto;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class GroupController {
+    public List<GroupDto> getGroups(){
+        return new ArrayList<>();
+    }
+
+    public GroupDto getGroup(Long groupId){
+        return new GroupDto(1L, "Ubrania");
+    }
+
+    public void createGroup(GroupDto groupDto){
+        //do nothing
+    }
+
+    public GroupDto updateGroup(GroupDto groupDto){
+        return new GroupDto(1L, "Dodatki");
+    }
 }
