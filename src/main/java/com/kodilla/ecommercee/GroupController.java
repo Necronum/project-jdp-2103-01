4dc2f32cdc6c89759246537b44c3dd66aba29d41
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-01 22:16:53 +0200

    GroupController (update)
    - dodano klasę Group
    - update GroupController

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,25 +1,30 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.GroupDto;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("v1/group")
 public class GroupController {
+    @GetMapping(value="getGroups")
     public List<GroupDto> getGroups(){
         return new ArrayList<>();
     }
 
+    @GetMapping(value="getGroup")
     public GroupDto getGroup(Long groupId){
         return new GroupDto(1L, "Ubrania");
     }
 
+    @PostMapping(value="createGroup")
     public void createGroup(GroupDto groupDto){
         //do nothing
     }
 
+    @PutMapping(value="updateGroup")
     public GroupDto updateGroup(GroupDto groupDto){
         return new GroupDto(1L, "Dodatki");
     }
