7373a13e42e5b0025991acb8bde95ac0ca5c1674
Maikody <73773432+Maikody@users.noreply.github.com>
2021-04-02 15:18:48 +0200

    OrderMapper deleted

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,57 +1,49 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.OrderDto;
-import com.kodilla.ecommercee.mapper.OrderMapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("v1/order")
 public class OrderController {
 
-    private final OrderMapper orderMapper;
-    private final GenericEntityRepository repository;
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<OrderDto> getAllOrders() {
-        List<GenericEntity> orders = repository.findAll();
-        return orderMapper.mapToOrderDtoList(orders);
+        return Collections.emptyList();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public OrderDto getOrderById(@PathVariable Long id) {
-        return repository.findById(id)
-                .map(orderMapper::mapToOrderDto)
-                .orElseThrow(() -> new IllegalStateException("Order not found"));
+        return new OrderDto(id, "Order");
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void addOrder(@RequestBody OrderDto orderDto) {
-        GenericEntity order = orderMapper.mapToOrder(orderDto);
-        repository.save(order);
+        log.info("Order saved in database");
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void updateOrder(@PathVariable Long id, @RequestBody OrderDto orderDto) {
-        repository.findById(id)
-                .ifPresent(order -> {
-                    OrderDto.updateFields(orderDto, order);
-                    repository.save(order);
-                });
+    public OrderDto updateOrder(@PathVariable Long id, @RequestBody OrderDto orderDto) {
+        log.info("Order " + id + " updated");
+        return orderDto;
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeOrderById(@PathVariable Long id) {
-        repository.deleteById(id);
+        log.info("Order " + id + " removed from database");
     }
 
 }
--- src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -1,21 +1,13 @@
 package com.kodilla.ecommercee.domain;
 
-import com.kodilla.ecommercee.GenericEntity;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
 @AllArgsConstructor
 public class OrderDto {
     private final Long id;
     private final String value;
-
-    public static void updateFields(OrderDto orderDto, GenericEntity order) {
-        if (orderDto.getValue() != null) {
-            order.setValue(orderDto.getValue());
-        }
-    }
-
 }
