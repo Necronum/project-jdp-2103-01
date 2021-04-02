e5ca533528c98fb832952f4fb4f49666af777698
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-02 16:40:05 +0200

    0.2 Added Requests
    - added RequestBody and RequestParam

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/group")
+@RequestMapping("/v1/group")
 public class GroupController {
     @GetMapping(value="getGroups")
     public List<GroupDto> getGroups(){
@@ -15,17 +15,17 @@ public class GroupController {
     }
 
     @GetMapping(value="getGroup")
-    public GroupDto getGroup(Long groupId){
+    public GroupDto getGroup(@RequestParam Long groupId){
         return new GroupDto(1L, "Ubrania");
     }
 
     @PostMapping(value="createGroup")
-    public void createGroup(GroupDto groupDto){
+    public void createGroup(@RequestBody GroupDto groupDto){
         //do nothing
     }
 
     @PutMapping(value="updateGroup")
-    public GroupDto updateGroup(GroupDto groupDto){
-        return new GroupDto(1L, "Dodatki");
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto){
+        return new GroupDto(1L, "Updated group");
     }
 }
