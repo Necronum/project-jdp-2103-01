ef8de5564303dd687d8a995244a36af8da65205f
Grzegorz <karaluch26@wp.pl>
2021-04-02 17:35:32 +0200

    added product entity

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -2,12 +2,38 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCTS")
 public class Product {
 
     private Long id;
     private String productName;
 
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID")
+    public Long getId() {
+        return this.id;
+    }
+    @Column(name = "PRODUCT_NAME" )
+    public String getProductName() {
+        return this.productName;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setProductName(String productName) {
+        this.productName = productName;
+    }
 }
