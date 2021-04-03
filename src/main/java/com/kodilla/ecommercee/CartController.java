adbc18fdd87096aecbd899ddaeddec276ed6181f
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-03 15:29:10 +0200

    CartController update
    - changed lombok
    - added Slf4j

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,35 +1,45 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.CartDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@Slf4j
 @RestController
+@AllArgsConstructor
 @RequestMapping(value="/v1/cart")
 public class CartController {
-    @PostMapping(value="createEmptyCart")
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public CartDto createEmptyCart(){
         return new CartDto();
     }
 
-    @GetMapping(value="getCart")
-    public CartDto getCart(@RequestParam Long cartId){
+    @GetMapping("/{cartId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CartDto getCart(@PathVariable Long cartId){
         return new CartDto(1L);
     }
 
-    @PutMapping(value="addProductToCart")
-    public void addProductToCart(@RequestParam Long cartId, @RequestParam Long productId){
-        //do nothing
+    @PutMapping("/{cartId}?{productId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void addProductToCart(@PathVariable Long cartId, @PathVariable Long productId){
+        log.info("Product: " + productId + " has been added to cart: " + cartId);
     }
 
-    @DeleteMapping(value="deleteProductFromCart")
-    public void deleteProductFromCart(@RequestParam Long cartId, @RequestParam Long productId){
-        //do nothing
+    @DeleteMapping("/{cartId}?{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProductFromCart(@PathVariable Long cartId, @PathVariable Long productId){
+        log.info("Product: " + productId + " has been removed to cart: " + cartId);
     }
 
-    @PostMapping(value="createOrder")
-    public void createOrder(@RequestParam Long cartId){
-        //do nothing
+    @PostMapping("/{cartId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createOrder(@PathVariable Long cartId){
+        log.info("Order form cart: " + cartId + " has been added to car ");
     }
 }
