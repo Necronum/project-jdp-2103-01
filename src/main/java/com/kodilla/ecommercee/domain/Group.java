3781255b0de5b0a945b96068575a0c5ad97283fc
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-06 10:36:33 +0200

    Update with packages
    - reorganization within packages

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -3,22 +3,37 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity(name="group")
+@Entity(name="GROUPS")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Group {
-    @Column(name = "id", unique = true)
+
+    @Column(name = "ID", unique = true)
     @Id
     @GeneratedValue
+    @NotNull
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "NAME")
     private String name;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "ID",
+            fetch = FetchType.EAGER
+    )
+    private List<Product> products = new ArrayList<>();
+
+    public Group(String name){
+        this.name = name;
+    }
 }
