c28d754405d68de1c83fe6b7b7ecef3cd3795c67
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-06 10:56:30 +0200

    Product stub
    - product stub for proper implementation of group

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -2,12 +2,20 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
+    @Id
     private Long id;
+
     private String productName;
 
 }
