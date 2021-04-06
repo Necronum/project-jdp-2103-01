07f38636dc76628b27434dfb7364db42213b543d
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-06 08:02:30 +0200

    Encja Group - implementacja
    - implementacja encji Group

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -2,10 +2,22 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="group")
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Group {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String name;
 }
