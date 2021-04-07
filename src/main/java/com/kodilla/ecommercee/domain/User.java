3ee913935304624520047eb37645f3809b395eb5
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-04-07 16:45:14 +0200

    Entity Refactor
    - changed entity names and fields
    - fixed relations

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,10 +1,17 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class User {
+
     @Id
-    Long id;
+    @GeneratedValue
+    @NotNull
+    @Column(name = "USER_ID", unique = true)
+    private Long id;
 }
