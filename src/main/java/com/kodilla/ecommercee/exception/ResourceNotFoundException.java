1d4122b67bb3b78078ce1e9714ed233869ed681d
Maikody <73773432+Maikody@users.noreply.github.com>
2021-04-23 14:02:00 +0200

    Response status added to ResourceNotFoundException

--- src/main/java/com/kodilla/ecommercee/exception/ResourceNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/exception/ResourceNotFoundException.java
@@ -1,8 +1,11 @@
 package com.kodilla.ecommercee.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
-
 }
